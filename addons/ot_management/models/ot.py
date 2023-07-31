1996a41344699eadc57a4117bf54277b1248116f
dat.phamthanh1 <dat.phamthanh1@vti.com.vn>
2023-07-31 14:17:24 +0700

    add request line id constraint, fix otmonth compute field

--- addons/ot_management/models/ot.py
+++ addons/ot_management/models/ot.py
@@ -1,37 +1,41 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+from datetime import datetime
 
 
 class Registration(models.Model):
     _name = 'ot.registration'
     _description = 'Registration information'
 
-    @api.onchange('project_id')
-    def _onchange_project_id(self):
+    @api.onchange('project_id', 'employee_id')
+    def _onchange_display_name(self):
         for record in self:
-            if record.project_id:
+            if record.project_id and record.employee_id:
                 record.name = record.employee_id.name + ' - ' + record.project_id.name
-    name = fields.Char(default='OT Registration')
+    name = fields.Char(default='')
     state = fields.Selection(
-        [('draft','Draft'),
-         ('to approve','To Approve'),
-         ('pm approved','PM Approved'),
-         ('dl approved','DL Approved'),
-         ('refused','Refused')],
-        default='draft'
+        [('draft', 'Draft'),
+         ('to approve', 'To Approve'),
+         ('pm approved', 'PM Approved'),
+         ('dl approved', 'DL Approved'),
+         ('refused', 'Refused')],
+        default='draft',
+        required=True
     )
     project_id = fields.Many2one('project.project', required=True)
 
     def _get_employee_id(self):
-        employee = self.env['hr.employee'].search([('user_id','=',self.env.uid)], limit=1)
+        employee = self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1)
         return employee
+
     employee_id = fields.Many2one('hr.employee',
                                   default=_get_employee_id,
-                                  readonly=True)
+                                  readonly=True,
+                                  required=True)
 
     lead_id = fields.Many2one(
         'hr.employee',
-        string='Department Lead',
+        'Department lead',
         related='employee_id.parent_id')
 
     @api.depends('lead_id', 'project_id')
@@ -42,14 +46,15 @@ class Registration(models.Model):
             else:
                 if record.state == 'draft' or record.state == 'to approve':
                     record.approve_id = self.env['hr.employee'].search(
-                        [('user_id','=',record.project_id.user_id.id)],
+                        [('user_id', '=', record.project_id.user_id.id)],
                         limit=1)
                 elif record.state == 'pm approved':
                     record.approve_id = record.lead_id
                 elif record.state == 'dl approved' or record.state == 'refused':
                     record.approve_id = False
+
     approve_id = fields.Many2one('hr.employee',
-                                 string='Approver',
+                                 'Approver',
                                  compute='_compute_approver',
                                  store=True,
                                  readonly=False,
@@ -57,15 +62,25 @@ class Registration(models.Model):
     request_line_ids = fields.One2many('ot.request.line',
                                        'registration_id',
                                        ondelete='cascade')
-    total_ot = fields.Integer('Total OT', readonly=True)
+
+    @api.depends('request_line_ids')
+    def _compute_total_ot(self):
+        for record in self:
+            if record.request_line_ids:
+                res = 0
+                for line in record.request_line_ids:
+                    res += line.ot_hours
+                record.total_ot = res
+
+    total_ot = fields.Integer('Total OT hours', compute='_compute_total_ot', store=True)
 
     @api.depends('request_line_ids')
     def _compute_ot_month(self):
         for record in self:
             if record.request_line_ids:
-                req_line = self.env['ot.request.line'].browse(record.request_line_ids.ids, limit=1)
-                record.ot_month = req_line.start_time
-    ot_month = fields.Date('OT Month', compute='_compute_ot_month')
+                record.ot_month = record.request_line_ids[0].start_time.date()
+
+    ot_month = fields.Date('OT month', compute='_compute_ot_month', store=True)
 
     def submit_draft(self):
         for record in self:
@@ -74,11 +89,16 @@ class Registration(models.Model):
         return True
 
     @api.model
-    def create(self, vals_list):
-        res = super().create(vals_list)
-        if not res.request_line_ids:
-            raise ValidationError('At least one corresponding request line must be created')
-        return res
+    def create(self, values):
+        if 'request_line_ids' not in values:
+            values['request_line_ids'] = False
+        return super(Registration, self).create(values)
+
+    @api.constrains('request_line_ids')
+    def _check_request_lines(self):
+        for record in self:
+            if not record.request_line_ids:
+                raise ValidationError('At least one corresponding request line must be created')
 
 
 class RequestLine(models.Model):
@@ -86,28 +106,47 @@ class RequestLine(models.Model):
     _description = 'Request line information'
 
     name = fields.Char(related='registration_id.name')
-    start_time = fields.Datetime(default=fields.Datetime().today(), required=True)
-    end_time = fields.Datetime(default=fields.Datetime().today(), required=True)
+    start_time = fields.Datetime('From', default=fields.Datetime().today(), required=True)
+    end_time = fields.Datetime('To', default=fields.Datetime().today(), required=True)
+
+    @api.depends('start_time', 'end_time')
+    def _compute_ot_category(self):
+        for record in self:
+            if record.start_time.strftime('%a') == "Sun":
+                record.ot_category = 'weekend'
+            else:
+                record.ot_category = 'workday'
+
     ot_category = fields.Selection(
-        [('undefined','Undefined'),
-         ('ordinary day', 'Ordinary day'),
-         ('holiday', 'Holiday')]
+        [('undefined', 'Undefined'),
+         ('weekend', 'Weekend'),
+         ('workday', 'Workday')],
+        'OT category',
+        default='undefined',
+        compute='_compute_ot_category',
+        store=True
     )
     from_home = fields.Boolean(string='WFH')
-    ot_hours = fields.Float()
-    job_taken = fields.Char()
+
+    @api.depends('start_time', 'end_time')
+    def _compute_ot_hours(self):
+        for record in self:
+            record.ot_hours = int((record.end_time - record.start_time).total_seconds() / 3600.0)
+
+    ot_hours = fields.Integer('OT hours', compute='_compute_ot_hours', store=True)
+    job_taken = fields.Char('Job taken', default='N/A', required=True)
     state = fields.Selection(
-        [('draft','Draft'),
-         ('to approve','To Approve'),
-         ('pm approved','PM Approved'),
-         ('dl approved','DL Approved'),
-         ('refused','Refused')],
+        [('draft', 'Draft'),
+         ('to approve', 'To Approve'),
+         ('pm approved', 'PM Approved'),
+         ('dl approved', 'DL Approved'),
+         ('refused', 'Refused')],
         related='registration_id.state',
         store=True)
-    late_approved = fields.Boolean()
-    hr_notes = fields.Text()
-    attendance_notes = fields.Text()
-    warning = fields.Char()
+    late_approved = fields.Boolean('Late approved')
+    hr_notes = fields.Text('HR notes', readonly=True)
+    attendance_notes = fields.Text('Attendance notes', readonly=True)
+    warning = fields.Char(default='Exceed OT plan', readonly=True)
     registration_id = fields.Many2one('ot.registration')
     employee_id = fields.Many2one(related='registration_id.employee_id')
     project_id = fields.Many2one(related='registration_id.project_id')
@@ -116,7 +155,21 @@ class RequestLine(models.Model):
     def _compute_intern(self):
         for record in self:
             record.is_intern = True
+
     is_intern = fields.Boolean(compute='_compute_intern')
+
+    # @api.constrains('start_time', 'end_time')
+    # def _check_valid_date(self):
+    #     for record in self:
+    #         from_time = datetime.strptime(str(record.start_time), "%Y-%m-%d %H:%M:%S")
+    #         from_compared = from_time.hour * 60 + from_time.minute
+    #         if record.start_time.date() != record.end_time.date() or \
+    #                 from_compared - 1050 <= 0:
+    #             raise ValidationError('Invalid OT')
+    #         elif (record.end_time - record.start_time).total_seconds() < 3600:
+    #             raise ValidationError('OT hours must be larger than 0')
+    #         elif (record.end_time - fields.Datetime.now()).total_seconds() > 0:
+    #             raise ValidationError('Cannot create a future plan for OT')
 
 
 class Employee(models.Model):
